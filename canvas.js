2e1eb0b7429a5c890d28129faf6baf31fe53050d
Charles Durflinger <Charles.durflinger@gmail.com>
2019-06-04 16:51:17 -0400

    Updated Remove shape function to redraw background. Updated some functions to ES6. Changed app name.

--- canvas.js
+++ canvas.js
@@ -1,15 +1,15 @@
-let canvas = document.querySelector('canvas');
-let heightSlider = document.getElementById('heightSlider');
-let widthSlider = document.getElementById('widthSlider');
-let navButton = document.getElementById('navButton');
-let navItems = document.getElementById('navItems');
-let shapeButtons = document.getElementsByTagName('button');
-let shapeDivs = document.getElementsByClassName('shapeDiv');
-let c = canvas.getContext('2d');
-let shape = "square";
-let height = 25;
-let width = 25;
-let drawnShapes = [];
+let canvas = document.querySelector('canvas'),
+c = canvas.getContext('2d'),
+heightSlider = document.getElementById('heightSlider'),
+widthSlider = document.getElementById('widthSlider'),
+navButton = document.getElementById('navButton'),
+navItems = document.getElementById('navItems'),
+shapeButtons = document.getElementsByTagName('button'),
+shapeDivs = document.getElementsByClassName('shapeDiv'),
+shape = "square",
+height = 25,
+width = 25,
+drawnShapes = [];
 
 //initialize the canvas width/height on load.
 canvas.width = window.innerWidth;
@@ -17,6 +17,11 @@ canvas.height = window.innerHeight;
 
 //Listen for the window size to change. Update canvas size accordingly.
 window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
 
 //Responsive Nav
 navButton.addEventListener("click", function(){
@@ -58,29 +63,29 @@ canvas.addEventListener("click", function(){
     }
 })
 
-function resizeCanvas(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
 
+//create the circle shape and call the createDiv function to create a new html element on the page
 function createCircle(){
     let color = randomColor();
     c.beginPath();
     c.arc(event.clientX, event.clientY, width, 0, 2 * Math.PI);
     c.fillStyle = color;
     c.fill();
-    drawnShapes.push({shape: "circle", x: event.clientX, y: event.clientY, w: width});
+    drawnShapes.push({shape: "circle", x: event.clientX, y: event.clientY, w: width, c: color});
     createDiv(color);
 }
 
+//create the square shape and call the createDiv function to create a new html element on the page.
 function createSquare(){
     let color = randomColor();
     c.fillStyle = color;
     c.fillRect(event.clientX, event.clientY, width, height);
-    drawnShapes.push({shape: "square", x: event.clientX, y: event.clientY, w: width, h: height});
+    drawnShapes.push({shape: "square", x: event.clientX, y: event.clientY, w: width, h: height, c: color});
     createDiv(color);
 }
 
+//create a div and add a click listener to that div for the newly created shape. Add a CSS transition to the
+//dynamically created div.
 function createDiv(color){
     let div = document.createElement('div');
     div.className = 'shapeDiv';
@@ -98,6 +103,7 @@ function createDiv(color){
     });
 }
 
+//remove the Div from the html as well as the shape. Remove the shape information from the drawnShapes array.
 function clearShape(div){
     for(let i = 0; i < shapeDivs.length; i++){
         if(shapeDivs[i] === div){
@@ -110,15 +116,31 @@ function clearShape(div){
                 c.clearRect(drawnShapes[i].x - drawnShapes[i].w - 1, drawnShapes[i].y - drawnShapes[i].w - 1, drawnShapes[i].w * 2 + 2, drawnShapes[i].w * 2 + 2);
                 // c.closePath();
             }
-                drawnShapes.splice(i, 1);
+            drawnShapes.splice(i, 1);
+        }
+        for(let j = 0; j < shapeDivs.length; j++){
+            if(j === 0){
+                c.clearRect(0, 0, canvas.width, canvas.height);
+            }
+            if(drawnShapes[j].shape === "square"){
+                c.fillStyle = drawnShapes[j].c;
+                c.fillRect(drawnShapes[j].x, drawnShapes[j].y, drawnShapes[j].w, drawnShapes[j].h);
+            } else if(drawnShapes[j].shape ==="circle"){
+                c.beginPath();
+                c.arc(drawnShapes[j].x, drawnShapes[j].y, drawnShapes[j].w, 0, 2 * Math.PI);
+                c.fillStyle = drawnShapes[j].c;
+                c.fill();
+            }
         }
     }
 }
 
-function randomColor(){
+//generate a random rgb color for the shapes.
+let randomColor = () => {
     return "rgb(" + randomColNum() + ", " + randomColNum() + ", " + randomColNum() + ")";
 }
 
-function randomColNum(){
+//generate a random rgb color for the randomColor function.
+let randomColNum = () => {
     return Math.floor(Math.random() * 256);
 }
