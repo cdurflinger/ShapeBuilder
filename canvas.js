3788375e8fd67994a91b7452b3b89ef3f61b0f9b
Charles Durflinger <Charles.durflinger@gmail.com>
2019-06-05 16:00:52 -0400

    Added new shape to HTML and added JS function for that shape. Created shape navigation on canvis via mouseinputs.

--- canvas.js
+++ canvas.js
@@ -21,9 +21,10 @@ window.addEventListener('resize', resizeCanvas, false);
 function resizeCanvas(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    reset();
 }
 
-//Responsive Nav
+//Responsive Nav.
 navButton.addEventListener("click", function(){
     if(navItems.style.display === "block"){
         navItems.style.display = "none";
@@ -31,6 +32,9 @@ navButton.addEventListener("click", function(){
         navItems.style.display = "block"
     }
 })
+
+//Listen for the mouse to move on the canvas and call the draw function.
+canvas.addEventListener("mousemove", setOutline, false);
 
 //Changes value of shape variable depending on what button is clicked. Default is square.
 for(let i = 0; i < shapeButtons.length; i++){
@@ -60,11 +64,12 @@ canvas.addEventListener("click", function(){
         createSquare();
     } else if(shape === "circle"){
         createCircle();
+    } else if(shape === "triangle"){
+        createTriangle();
     }
 })
 
-
-//create the circle shape and call the createDiv function to create a new html element on the page
+//create the circle shape and call the createDiv function to create a new html element on the page.
 function createCircle(){
     let color = randomColor();
     c.beginPath();
@@ -79,8 +84,21 @@ function createCircle(){
 function createSquare(){
     let color = randomColor();
     c.fillStyle = color;
-    c.fillRect(event.clientX, event.clientY, width, height);
+    c.fillRect(event.clientX - width / 2, event.clientY - height / 2, width, height);
     drawnShapes.push({shape: "square", x: event.clientX, y: event.clientY, w: width, h: height, c: color});
+    createDiv(color);
+}
+
+//create a triangle shape and call the createDiv function to create a new html element on the page.
+function createTriangle(){
+    let color = randomColor();
+    c.beginPath();
+    c.moveTo(event.clientX, event.clientY);
+    c.lineTo(Number(event.clientX) - Number(width), Number(event.clientY) + Number(height));
+    c.lineTo(Number(event.clientX) + Number(width), Number(event.clientY) + Number(height));
+    c.fillStyle = color;
+    c.fill();
+    drawnShapes.push({shape: "triangle", x: event.clientX, y: event.clientY, w: width, h: height, c: color});
     createDiv(color);
 }
 
@@ -97,9 +115,12 @@ function createDiv(color){
         div.textContent = "S";
     } else if(shape === "circle"){
         div.textContent = "C";
+    } else if(shape === "triangle"){
+        div.textContent = "T";
     }
     div.addEventListener('click', function(){
         clearShape(this);
+        reDraw();
     });
 }
 
@@ -112,26 +133,44 @@ function clearShape(div){
                 c.clearRect(drawnShapes[i].x, drawnShapes[i].y, drawnShapes[i].w, drawnShapes[i].h);               
             } else if(drawnShapes[i].shape === "circle"){
                 document.getElementById('shapeContainer').removeChild(shapeDivs[i]);
-                // c.beginPath();
                 c.clearRect(drawnShapes[i].x - drawnShapes[i].w - 1, drawnShapes[i].y - drawnShapes[i].w - 1, drawnShapes[i].w * 2 + 2, drawnShapes[i].w * 2 + 2);
-                // c.closePath();
+            } else if(drawnShapes[i].shape === "triangle"){
+                document.getElementById('shapeContainer').removeChild(shapeDivs[i]);
+                c.clearRect(drawnShapes[i].x - drawnShapes[i].w, drawnShapes[i].y + drawnShapes[i].h, drawnShapes[i].w, drawnShapes[i].h);  
             }
             drawnShapes.splice(i, 1);
         }
-        for(let j = 0; j < shapeDivs.length; j++){
-            if(j === 0){
-                c.clearRect(0, 0, canvas.width, canvas.height);
-            }
-            if(drawnShapes[j].shape === "square"){
-                c.fillStyle = drawnShapes[j].c;
-                c.fillRect(drawnShapes[j].x, drawnShapes[j].y, drawnShapes[j].w, drawnShapes[j].h);
-            } else if(drawnShapes[j].shape ==="circle"){
-                c.beginPath();
-                c.arc(drawnShapes[j].x, drawnShapes[j].y, drawnShapes[j].w, 0, 2 * Math.PI);
-                c.fillStyle = drawnShapes[j].c;
-                c.fill();
-            }
+    }
+}
+
+//Clears the canvas, loops through the existing shapeDivs and redraws the shape.
+function reDraw(){
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    for(let i = 0; i< shapeDivs.length; i++){
+        if(drawnShapes[i].shape === "square"){
+            c.fillStyle = drawnShapes[i].c;
+            c.fillRect(drawnShapes[i].x - drawnShapes[i].w / 2, drawnShapes[i].y - drawnShapes[i].h / 2, drawnShapes[i].w, drawnShapes[i].h);
+        } else if(drawnShapes[i].shape === "circle"){
+            c.beginPath();
+            c.arc(drawnShapes[i].x, drawnShapes[i].y, drawnShapes[i].w, 0, 2 * Math.PI);
+            c.fillStyle = drawnShapes[i].c;
+            c.fill();
+        } else if(drawnShapes[i].shape === "triangle"){
+            c.beginPath();
+            c.fillStyle = drawnShapes[i].c;
+            c.moveTo(drawnShapes[i].x, drawnShapes[i].y);
+            c.lineTo(Number(drawnShapes[i].x) - Number(drawnShapes[i].w), Number(drawnShapes[i].y) + Number(drawnShapes[i].h));
+            c.lineTo(Number(drawnShapes[i].x) + Number(drawnShapes[i].w), Number(drawnShapes[i].y) + Number(drawnShapes[i].h));
+            c.fill();
         }
+    }
+}
+
+function reset(){
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    for(let i = 0; i < shapeDivs.length; i++){
+        document.getElementById('shapeContainer').removeChild(shapeDivs[i]);
+        drawnShapes.splice(i, 1);
     }
 }
 
@@ -143,4 +182,24 @@ let randomColor = () => {
 //generate a random rgb color for the randomColor function.
 let randomColNum = () => {
     return Math.floor(Math.random() * 256);
+}
+
+//create a transparent shape to show user where the placed shape will land.
+function setOutline(e) {
+    reDraw();
+    c.fillStyle = "rgba(255,255,51,0.6)";
+    if(shape === "square"){
+        c.fillRect(event.clientX - width / 2, event.clientY - height / 2, width, height);
+        c.fill();
+    } else if(shape === "circle"){
+        c.beginPath();
+        c.arc(event.clientX, event.clientY, width, 0, 2 * Math.PI);
+        c.fill();
+    } else if(shape === "triangle"){
+        c.beginPath();
+        c.moveTo(event.clientX, event.clientY);
+        c.lineTo(Number(event.clientX) - Number(width), Number(event.clientY) + Number(height));
+        c.lineTo(Number(event.clientX) + Number(width), Number(event.clientY) + Number(height));
+        c.fill();
+    }
 }
